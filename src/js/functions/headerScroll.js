1fee47dee4adcf604509baf9b7c84e746ed9919f
unknown <D:\The Bat!>
2022-05-25 17:47:08 +0300

    шаблон шапки

--- src/js/functions/headerScroll.js
+++ src/js/functions/headerScroll.js
@@ -2,13 +2,15 @@
 // Шапка при скролле
 //================================================================
 export const headerScroll = () => {
-    const header = document.querySelector('.header');
+    const header = document.querySelector('[data-scroll]');
+    console.log(header);
+
     if(header){
         let lastScroll = 0;
         const defaultOffset = 150;
         const scrollPosition = () => window.scrollY || document.documentElement.scrollTop;
         const containHide = () => header.classList.contains('scroll--active');
-        window.addEventListener('scroll--active', () => {
+        window.addEventListener('scroll', () => {
             if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
                 //scroll down
                 header.classList.add('scroll--active');
